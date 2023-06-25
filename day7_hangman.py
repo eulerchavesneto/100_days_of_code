76cd85609386de7f89c9e5f79a1dc6807f1adabf
Euler Chaves Neto <euler.neto@gmail.com>
2023-06-24 23:06:16 -0300

    commit

--- day7_hangman.py
+++ day7_hangman.py
@@ -2,6 +2,7 @@ import random
 import day7_hangman_wordlist
 import day7_hangman_logo
 
+
 print(day7_hangman_logo.logo)
 categoria = input("\nSeja bem vindo(a) ao jogo da forca!\nEscolha uma categoria para jogar:\nFRUTAS, PAÍSES OU ANIMAIS?\nOu, se quiser jogar no modo super difícil em inglês, digite ENGLISH!\n").lower()
 if categoria == "frutas":
@@ -13,9 +14,6 @@ elif categoria == "animais":
 else:
     categoria = day7_hangman_wordlist.english
 chosen_word = random.choice(categoria)
-
-
-print(categoria)
 
 
 stages = ['''
@@ -75,11 +73,8 @@ stages = ['''
 =========
 ''']
 
-
-#Testing code
-print(f'Pssst, the solution is {chosen_word}.')
-
 display = []
+letras_chutadas = []
 length = len(chosen_word)
 lives = 6
 
@@ -90,7 +85,6 @@ print(display)
 
 #tentativas para adivinhar a palavra
 
-
 end_of_game = False
 
 while not end_of_game:
@@ -100,11 +94,19 @@ while not end_of_game:
         if letter == guess:
             display[position] = letter
 
+
+
     if guess not in chosen_word:
-        lives -= 1
-        if lives == 0:
-            end_of_game = True
-            print("SINTO MUITO... VOCÊ PERDEU!")
+        if guess in letras_chutadas:
+            print(f"Você já chutou essa letra e não perdeu uma vida! Até agora você já escolheu as seguintes letras: {letras_chutadas}")
+
+        else:
+            lives -= 1
+            print(f"Você chutou a letra {guess}, mas ela não contém na palavra. Você perdeu uma vida.")
+            letras_chutadas += guess
+            if lives == 0:
+                end_of_game = True
+                print(f"SINTO MUITO... VOCÊ PERDEU O JOGO! A palavra era {chosen_word.upper()} ;(")
 
     print(display)
     if "_" not in display:
