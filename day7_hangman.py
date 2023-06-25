69079f6d30455940c0364d2e3a9dc3099b37715f
Euler Chaves Neto <euler.neto@gmail.com>
2023-06-24 22:34:59 -0300

    commit

--- day7_hangman.py
+++ day7_hangman.py
@@ -1,6 +1,22 @@
 import random
-word_list = ["aardvark", "baboon", "camel"]
-chosen_word = random.choice(word_list)
+import day7_hangman_wordlist
+import day7_hangman_logo
+
+print(day7_hangman_logo.logo)
+categoria = input("\nSeja bem vindo(a) ao jogo da forca!\nEscolha uma categoria para jogar:\nFRUTAS, PAÍSES OU ANIMAIS?\nOu, se quiser jogar no modo super difícil em inglês, digite ENGLISH!\n").lower()
+if categoria == "frutas":
+    categoria = day7_hangman_wordlist.frutas
+elif categoria == "paises":
+    categoria = day7_hangman_wordlist.paises
+elif categoria == "animais":
+    categoria = day7_hangman_wordlist.animais
+else:
+    categoria = day7_hangman_wordlist.english
+chosen_word = random.choice(categoria)
+
+
+print(categoria)
+
 
 stages = ['''
   +---+
@@ -78,21 +94,23 @@ print(display)
 end_of_game = False
 
 while not end_of_game:
-    guess = input("Guess a letter: ").lower()
+    guess = input("Chute uma letra: ").lower()
     for position in range(length):
         letter = chosen_word[position]
         if letter == guess:
             display[position] = letter
-        else:
-            lives -= 1
-            print("Errou!")
+
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("SINTO MUITO... VOCÊ PERDEU!")
+
     print(display)
     if "_" not in display:
         end_of_game = True
-        print("You win!")
-    if lives == 0:
-        end_of_game = True
-        print("You lose!")
+        print("PARABÉNS! VOCÊ VENCEU O JOGO DA FORCA!")
+    print(stages[lives])
 
 
 
