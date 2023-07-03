5bf07f6351435c7c75ff2b774a9dbcb80d11d4d5
Euler Chaves Neto <euler.neto@gmail.com>
2023-07-02 23:40:38 -0300

    commit

--- day8_caesar_cipher.py
+++ day8_caesar_cipher.py
@@ -1,21 +1,37 @@
+import day8_caesar_cipher_art
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
             'q', 'r', 's', 't']
-
+print(day8_caesar_cipher_art.logo)
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
+shift = shift % 26
 
 def caesar(start_text, shift_amount, cipher_direction):
     end_text = ""
-    for letter in start_text:
-        position = alphabet.index(letter)
-        if cipher_direction == "decode":
-            shift_amount = abs(shift_amount)
-            shift_amount *= -1
-        new_position = position + shift_amount
-        end_text += alphabet[new_position]
-    print(f"The {cipher_direction}d text is {end_text}")
+    if cipher_direction == "decode":
+        shift_amount *= -1
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
+    print(f"Here's the {cipher_direction}d result: {end_text}")
 
+should_end = False
+while not should_end:
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  shift = shift % 26
+
+  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+  restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+  if restart == "no":
+    should_end = True
+    print("Goodbye")
 
---
+++ day9_secret_auction.py
@@ -0,0 +1,7 @@
+import os
+import day9_secret_auction_art
+logo = day9_secret_auction_art.logo
+print(logo)
+
+# Clearing the Screen
+os.system('cls')
